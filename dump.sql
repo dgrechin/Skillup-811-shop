876ab3a556ff33cd0c76221d69cbeab09971e50b
Iforfreee <igor.freee88@gmail.com>
2019-01-21 21:26:51 +0200

    Переводы, авторизация. добавили авторизацию по форме

--- dump.sql
+++ dump.sql
@@ -47,7 +47,8 @@ DROP TABLE IF EXISTS `migration_versions`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `migration_versions` (
-  `version` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `version` varchar(14) COLLATE utf8_unicode_ci NOT NULL,
+  `executed_at` datetime DEFAULT NULL COMMENT '(DC2Type:datetime_immutable)',
   PRIMARY KEY (`version`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -58,7 +59,7 @@ CREATE TABLE `migration_versions` (
 
 LOCK TABLES `migration_versions` WRITE;
 /*!40000 ALTER TABLE `migration_versions` DISABLE KEYS */;
-INSERT INTO `migration_versions` VALUES ('20190114174339'),('20190114185659'),('20190116173334'),('20190116182531');
+INSERT INTO `migration_versions` VALUES ('20190114174339',NULL),('20190114185659',NULL),('20190116173334',NULL),('20190116182531',NULL),('20190121174639','2019-01-21 17:47:38'),('20190121174907','2019-01-21 17:49:10'),('20190121183845','2019-01-21 18:39:58');
 /*!40000 ALTER TABLE `migration_versions` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -88,8 +89,45 @@ CREATE TABLE `products` (
 
 LOCK TABLES `products` WRITE;
 /*!40000 ALTER TABLE `products` DISABLE KEYS */;
-INSERT INTO `products` VALUES (1,'Телевизор','Лучший телек',15000,1,3),(2,'Ноутбук','Лучший ноут',12000,1,2),(3,'Пылесос','бест пул',NULL,0,1);
+INSERT INTO `products` VALUES (1,'Телевизор','Лучший телевизор',15000,1,3),(2,'Ноутбук','Лучший ноут',12000,1,2),(3,'Пылесос','бест пул',NULL,0,1);
 /*!40000 ALTER TABLE `products` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(180) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `roles` longtext COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '(DC2Type:array)',
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `username` varchar(180) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `username_canonical` varchar(180) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email_canonical` varchar(180) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `enabled` tinyint(1) NOT NULL,
+  `salt` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `last_login` datetime DEFAULT NULL,
+  `confirmation_token` varchar(180) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `password_requested_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UNIQ_1483A5E992FC23A8` (`username_canonical`),
+  UNIQUE KEY `UNIQ_1483A5E9A0D96FBF` (`email_canonical`),
+  UNIQUE KEY `UNIQ_1483A5E9C05FB297` (`confirmation_token`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'test@test.com','','$argon2i$v=19$m=1024,t=2,p=2$Ny5vdjRtVkdmSTRvUjVJWA$kXnZRZpfZIaQnyTn62izpJcVuOWF1Q117MHMVX/u3vY','','','',0,NULL,NULL,NULL,NULL),(2,'admin@test.com','a:2:{i:0;s:5:\"ADMIN\";i:1;s:10:\"ROLE_ADMIN\";}','$argon2i$v=19$m=1024,t=2,p=2$WGJPVjFWR2VneTZUSVRhUQ$nwaxiKayzsn7kF7hXPl9McI5D/8PbbLayzWUJGw8IVo','admin@test.com','admin@test.com','admin@test.com',1,'eOA4rRY9WsA8S.7.xCluixzloSwQ5.FuIaYnD7u4l3s','2019-01-21 21:17:26',NULL,NULL);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -101,4 +139,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-01-16 20:35:47
+-- Dump completed on 2019-01-21 21:26:38
